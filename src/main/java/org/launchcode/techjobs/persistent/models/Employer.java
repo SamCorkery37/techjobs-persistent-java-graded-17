18f12c1161fc26305c0e1ae03c4b1196cd040905
Sam Corkery <134240832+Piemaster37@users.noreply.github.com>
2023-11-10 11:43:35 -0600

    newcommit

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,12 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Employer extends AbstractEntity {
    @Size(max = 250, message = "That doesnt exist")
    @NotBlank(message = "Enter a location")
     private String location;
+
+   public Employer () {
+
+   }
 
    public String getLocation() {
        return location;
